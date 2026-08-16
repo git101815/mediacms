983618e737765c60a0997b2cf92dae12e2cf7dac
jeanvaljean101815 <jeanvaljean101815@proton.me>
2026-08-16 15:29:56 +0300

    fix encoding tests

--- tests/api/test_new_media.py
+++ tests/api/test_new_media.py
@@ -1,8 +1,9 @@
 import uuid
+from unittest.mock import patch
 
-from django.test import Client, TestCase
+from django.test import Client, TestCase, override_settings
 
-from files.models import Encoding, Media
+from files.models import Media
 from files.tests import create_account
 
 API_V1_LOGIN_URL = '/api/v1/login'
@@ -16,16 +17,14 @@ class TestX(TestCase):
 
         self.user = create_account(password=self.password)
 
-    def test_file_upload(self):
+    @override_settings(REMOTE_ENCODING_ENABLED=True)
+    @patch("files.tasks.submit_remote_encoding.apply_async")
+    def test_file_upload(self, submit_remote_encoding):
         client = Client()
         client.login(username=self.user.username, password=self.password)
 
-        # use both ways, form + API to upload a new media file
-        # while video transcoding through ffmpeg takes place asynchronously
-        # (through celery workers), inside tests ffmpeg runs synchronously
-        # because celery is started with setting task_always_eager
-        # practically this means that this testing will take some time, but
-        # ensures that video transcoding completes well
+        # Use both upload paths. Video transcoding is remote, so this test only
+        # verifies upload/media detection and that each video is queued for Runpod.
         with open('fixtures/small_video.mp4', 'rb') as fp:
             client.post('/api/v1/media', {'title': 'small video file test', 'media_file': fp})
 
@@ -38,13 +37,20 @@ class TestX(TestCase):
         self.assertEqual(Media.objects.all().count(), 3, "Problem with file upload")
         # by default the portal_workflow is public, so anything uploaded gets public
         self.assertEqual(Media.objects.filter(state='public').count(), 3, "Expected all media to be public, as per the default portal workflow")
-        self.assertEqual(Media.objects.filter(media_type='video', encoding_status='success').count(), 2, "Encoding did not finish well")
+        self.assertEqual(Media.objects.filter(media_type='video', encoding_status='running').count(), 2, "Video uploads were not queued for remote encoding")
         self.assertEqual(Media.objects.filter(media_type='video').count(), 2, "Media identification failed")
         self.assertEqual(Media.objects.filter(media_type='image').count(), 1, "Media identification failed")
         self.assertEqual(Media.objects.filter(user=self.user).count(), 3, "User assignment failed")
-        medium_video = Media.objects.get(title="medium_video.mp4")
-        self.assertEqual(len(medium_video.hls_info), 13, "Problem with HLS info")
 
-        # using the provided EncodeProfiles, these two files should produce 9 Encoding objects.
-        # if new EncodeProfiles are added and enabled, this will break!
-        self.assertEqual(Encoding.objects.filter(status='success').count(), 10, "Not all video transcodings finished well")
+        self.assertEqual(submit_remote_encoding.call_count, 2, "Each uploaded video should queue one remote encoding job")
+        scheduled_tokens = {
+            call.kwargs["args"][0]
+            for call in submit_remote_encoding.call_args_list
+        }
+        video_tokens = set(
+            Media.objects.filter(media_type='video').values_list(
+                'friendly_token',
+                flat=True,
+            )
+        )
+        self.assertEqual(scheduled_tokens, video_tokens, "Remote encoding was not queued for every uploaded video")
